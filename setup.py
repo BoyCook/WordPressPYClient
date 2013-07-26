55dffa5a38b4c6347702899498e21c01cef81786
Craig Cook <boycook@me.com>
2013-07-26 16:24:55 +0100

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(
     description="A WordPress client written in Python",
     long_description=readme,
     author="Craig Cook",
-    packages=['wordpress-py-client'],
     url="https://github.com/BoyCook/WordPressPYClient",
     packages=find_packages(exclude=['test'])
 )
