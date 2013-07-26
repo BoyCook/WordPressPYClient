e77628e0131b115db40f1c50e3d38e8756fb920f
Craig Cook <boycook@me.com>
2013-07-26 17:17:14 +0100

    Using objects in test

--- test/test_wordpress.py
+++ test/test_wordpress.py
@@ -3,16 +3,30 @@
 import unittest
 from wordpress.wordpress import WordPress
 
+
 class WordPressTest(unittest.TestCase):
-    
     def setUp(self):
         self.blog = WordPress('boycook.wordpress.com')
-    
+
     def test_get_posts(self):
-        self.blog.get_posts()
+        posts = self.blog.get_posts()
+        for post in posts["posts"]:
+            id = post['ID']
+            print 'ID' + str(id)
 
     def test_get_post(self):
-        self.blog.get_post('100')
+        # TODO - assertions
+        post = self.blog.get_post('100')
+        id = post['ID']
+        print 'ID' + str(id)
+        # post['content']
+        # post['modified']
+        # post['created_at']
+        # post['modified']
+        # post['excerpt']
+        # post['title']
+        # post['URL']
+
 
 if __name__ == '__main__':
     unittest.main()
