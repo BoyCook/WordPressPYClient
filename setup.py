86d47e58e5e9d97f2072409fd4b1ffc73e091283
Craig Cook <boycook@me.com>
2013-09-15 15:59:06 +0100

    Removed duplicate dep

--- setup.py
+++ setup.py
@@ -8,18 +8,18 @@ readme = open('README.md').read()
 VERSION = '0.0.1'
 REQUIREMENTS = {
 	'install': ['httplib2', 'json'],
-	'testing': ['pytest', 'httplib2', 'coverage', 'pytest-cov', 'python-coveralls'],
+	'testing': ['pytest', 'coverage', 'pytest-cov', 'python-coveralls'],
 }
 
 if __name__ == '__main__':
 	setup(
-    version=VERSION,
-    name="wordpress-py-client",
-    description="A WordPress client written in Python",
-    long_description=readme,
-    author="Craig Cook",
-    url="https://github.com/BoyCook/WordPressPYClient",
-    packages=find_packages(exclude=["test"]),
-    install_requires=REQUIREMENTS["install"],
-	extras_require= { "testing":  REQUIREMENTS["testing"] }
-)
+	    version=VERSION,
+	    name="wordpress-py-client",
+	    description="A WordPress client written in Python",
+	    long_description=readme,
+	    author="Craig Cook",
+	    url="https://github.com/BoyCook/WordPressPYClient",
+	    packages=find_packages(exclude=["test"]),
+	    install_requires=REQUIREMENTS["install"],
+		extras_require= { "testing":  REQUIREMENTS["testing"] }
+	)
